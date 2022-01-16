126bfaaaaeff2738afa0b94a58600da378efc3b2
minjun7984 <54847941+minjun7984@users.noreply.github.com>
2022-01-16 23:18:36 +0900

    controller 구현

---
+++ src/main/java/com/example/test/controller/MemberController.java
@@ -0,0 +1,44 @@
+package com.example.test.controller;
+
+import com.example.test.domain.Member;
+import com.example.test.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller //스프링 컨테이너가 관리 객체가 생성되어 보관 new없이 생성가능
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired //스프링 컨테이너의 객체와 연결
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
